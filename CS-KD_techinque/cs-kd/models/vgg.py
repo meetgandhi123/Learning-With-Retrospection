a2ad1bcc19ab46625b8e77943cda4028245169c3
Robert Thomas <robert.jeffrey.thomas@gmail.com>
2022-11-02 16:10:51 -0400

    adjusting model parameters to match other models

--- CS-KD_techinque/cs-kd/models/vgg.py
+++ CS-KD_techinque/cs-kd/models/vgg.py
@@ -4,14 +4,12 @@ from torchvision import models
 
 __all__ = ['CIFAR10_VGG16', 'CIFAR100_VGG16']
 
-def CIFAR10_VGG16():
-    num_classes = 10
+def CIFAR10_VGG16(pretrained, num_classes, **kwargs):
     model = models.vgg16()
     model.classifier[6] = nn.Linear(4096,num_classes)
     return model
 
-def CIFAR100_VGG16():
-    num_classes = 100
+def CIFAR100_VGG16(pretrained, num_classes, **kwargs):
     model = models.vgg16()
     model.classifier[6] = nn.Linear(4096,num_classes)
     return model
