273b3ccf757128c950fac5ae1ad790fafc372867
Robert Thomas <robert.jeffrey.thomas@gmail.com>
2022-11-03 11:42:25 -0400

    modifying vgg implementation

--- CS-KD_techinque/cs-kd/models/vgg.py
+++ CS-KD_techinque/cs-kd/models/vgg.py
@@ -5,7 +5,7 @@ from torchvision import models
 __all__ = ['CIFAR10_VGG16', 'CIFAR100_VGG16']
 
 def CIFAR10_VGG16(pretrained, num_classes, **kwargs):
-    model = models.vgg16_bn(**kwargs)
+    model = models.vgg16_bn(weights='DEFAULT', **kwargs)
     num_ftrs = model.classifier[6].in_features
     model.classifier[6] = nn.Linear(num_ftrs,num_classes)
     return model
